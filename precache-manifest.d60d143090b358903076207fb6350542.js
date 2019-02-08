b482358e30ef50494b58aadc81e996f7727e8ab1
Dominique Nguyen <domnguyen@gmail.com>
2019-02-07 22:52:49 -0800

    Updates

---
+++ precache-manifest.d60d143090b358903076207fb6350542.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/landingpage/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3bd09c43796294cb8952",
+    "url": "/landingpage/static/js/runtime~main.3bd09c43.js"
+  },
+  {
+    "revision": "504c517ba1c0105fa3e1",
+    "url": "/landingpage/static/js/main.504c517b.chunk.js"
+  },
+  {
+    "revision": "13eeb2032981bf3107f9",
+    "url": "/landingpage/static/js/1.13eeb203.chunk.js"
+  },
+  {
+    "revision": "504c517ba1c0105fa3e1",
+    "url": "/landingpage/static/css/main.588b1185.chunk.css"
+  },
+  {
+    "revision": "2f2f746063a8d8b2e4ceebbda14bfb97",
+    "url": "/landingpage/index.html"
+  }
+];
